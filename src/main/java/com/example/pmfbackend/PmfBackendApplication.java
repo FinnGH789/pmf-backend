759409af1fe82611e8bc3a8aaec61b4ae44c83f3
finn <finnb@freenet.de>
2024-10-27 20:32:10 +0100

    Initial Commit

---
+++ src/main/java/com/example/pmfbackend/PmfBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pmfbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmfBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmfBackendApplication.class, args);
+    }
+
+}
