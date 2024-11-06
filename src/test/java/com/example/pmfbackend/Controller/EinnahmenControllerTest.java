9e16b73baac917aad877b8b197eba6d9a95f1880
finn <finnb@freenet.de>
2024-11-06 17:46:04 +0100

    Test Ausgelagert von Controller in den Service

--- src/test/java/com/example/pmfbackend/Controller/EinnahmenControllerTest.java
+++ src/test/java/com/example/pmfbackend/Controller/EinnahmenControllerTest.java
@@ -1,42 +1,8 @@
 package com.example.pmfbackend.Controller;
 
-import com.example.pmfbackend.Entity.Einnahmen;
-import com.example.pmfbackend.Repository.EinnahmenRepository;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import java.util.Arrays;
-
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 public class EinnahmenControllerTest {
 
-    @Autowired
-    private EinnahmenController einnahmenController;
-
-    @MockBean
-    private EinnahmenRepository einnahmenRepository;
-
-    @BeforeEach
-    void setup() {
-        Einnahmen einnahme1 = new Einnahmen(1L, "Freelance", 500);
-        Einnahmen einnahme2 = new Einnahmen(2L, "Dividende", 300);
-        Einnahmen einnahme3 = new Einnahmen(2L, "Dividende", 800);
-        Einnahmen einnahme4 = new Einnahmen(2L, "Dividende", 600);
-        Einnahmen einnahme5 = new Einnahmen(2L, "Dividende", 200);
-
-        when(einnahmenRepository.findAll()).thenReturn(Arrays.asList(einnahme1, einnahme2, einnahme3, einnahme4, einnahme5));
-    }
-
-    @Test
-    public void testGetSumEinnahmen() {
-        einnahmenController.getSumEinnahmen();
-
-        verify(einnahmenRepository).findAll();
-    }
 }
---
+++ src/test/java/com/example/pmfbackend/Service/EinnahmenServiceTest.java
@@ -0,0 +1,40 @@
+package com.example.pmfbackend.Service;
+
+import com.example.pmfbackend.Entity.Einnahmen;
+import com.example.pmfbackend.Repository.EinnahmenRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Arrays;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+class EinnahmenServiceTest {
+
+    @Autowired
+    private EinnahmenService einnahmenService;
+
+    @MockBean
+    private EinnahmenRepository einnahmenRepository;
+
+    @BeforeEach
+    void setup() {
+        Einnahmen einnahme1 = new Einnahmen(1L, "Freelance", 500);
+        Einnahmen einnahme2 = new Einnahmen(2L, "Dividende", 300);
+        Einnahmen einnahme3 = new Einnahmen(2L, "Erbe", 800);
+        Einnahmen einnahme4 = new Einnahmen(2L, "Gehalt", 600);
+        Einnahmen einnahme5 = new Einnahmen(2L, "Taschengeld", 200);
+
+        when(einnahmenRepository.findAll()).thenReturn(Arrays.asList(einnahme1, einnahme2, einnahme3, einnahme4, einnahme5));
+    }
+
+    @Test
+    public void getTotalEinnahmenTest() {
+        einnahmenService.getTotalEinnahmen();
+
+        verify(einnahmenRepository).findAll();
+    }
+}
